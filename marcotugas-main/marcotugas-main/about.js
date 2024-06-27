99c3a7cd91a4c8cb4721bae4a754beee777487fa
Fuyunot4u <ufuyuu18@gmail.com>
2024-06-27 15:33:20 +0700

    Delete marcotugas-main/marcotugas-main/about.js

--- marcotugas-main/marcotugas-main/about.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.getElementById('hamburger');
-    const mainSidebar = document.getElementById('main-sidebar');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('active');
-        mainSidebar.classList.toggle('active');
-    });
-});
